9a49f604a67ddf0321dc1916d168b3f390c7da65
dany-salazar <danysalazar1985@gmail.com>
2025-11-25 11:09:13 -0500

    CAMBIOS beneficiarios

--- js/utils.js
+++ js/utils.js
@@ -91,14 +91,20 @@ function addPopupToMap(nombreCapa) {
      
       var text = `
       <b>id:</b> ${props["id"] || "N/A"}<br>
-      <b>Proyecto:</b> ${props["beneficiar"] || "N/A"}<br>
-      <b>Parroquia:</b> ${props["benefici_4"] || "N/A"}<br>
-        <b>Fecha de ingreso:</b> ${props["benefici_1"] || "N/A"}<br>
-         <b>Embarazada:</b> ${props["benefici_7"] || "N/A"}<br>
-           <b>Semanas de gestion:</b> ${props["benefici_8"] || "N/A"}<br>
-           <b>Bono Bies:</b> ${props["benefici_9"] || "N/A"}<br>
-           <b>Nivel de Educacion:</b> ${props["benefici11"] || "N/A"}<br>
-
+      <b>Proyecto:</b> ${props["b_Field2"] || "N/A"}<br>
+      <b>Parroquia:</b> ${props["b_Field6"] || "N/A"}<br>
+      <b>Dirección:</b> ${props["b_Field7"] || "N/A"}<br>
+       <b>Área:</b> ${props["b_Field8"] || "N/A"}<br>
+        <b>Fecha de ingreso:</b> ${props["b_Field3"] || "N/A"}<br>
+         <b>Embarazada:</b> ${props["b_Field9"] || "N/A"}<br>
+           <b>Edad gestacional:</b> ${props["b_Field10"] || "N/A"}<br>
+           <b>Bono Bies:</b> ${props["b_Field11"] || "N/A"}<br>
+           <b>Edad cumplido:</b> ${props["b_Field12"] || "N/A"}<br>
+           <b>Nivel de Educacion:</b> ${props["b_Field13"] || "N/A"}<br>
+           <b>% de Discapacidad:</b> ${props["b_Field16"] || "N/A"}<br>
+           <b>Hijos menores a 17 años 11 meses:</b> ${props["b_Field18"] || "N/A"}<br>
+            <b>Hijos menores a 2 años:</b> ${props["b_Field19"] || "N/A"}<br>
+ <b>Material de la vivienda:</b> ${props["b_Field28"] || "N/A"}<br>
 
     `;
 
